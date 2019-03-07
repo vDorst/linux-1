5bce54a1ad0da6e4721329f9f126bb0eabf29fb9
René van Dorst <opensource@vdorst.com>
2019-03-08 00:56:09 +0100

    net: mtk_eth_soc, mt7530: suppport pause.
    Signed-off-by: René van Dorst <opensource@vdorst.com>

--- drivers/net/ethernet/mediatek/mtk_eth_soc.c
+++ drivers/net/ethernet/mediatek/mtk_eth_soc.c
@@ -243,6 +243,46 @@ static void mtk_phy_link_adjust(struct net_device *dev)
 	mtk_w32(mac->hw, mcr, MTK_MAC_MCR(mac->id));
 }
 
+static void mtk_gmac_sgmii_hw_setup(struct mtk_eth *eth, int mac_id)
+{
+	u32 val;
+
+	/* Setup the link timer and QPHY power up inside SGMIISYS */
+	regmap_write(eth->sgmiisys, SGMSYS_PCS_LINK_TIMER,
+		     SGMII_LINK_TIMER_DEFAULT);
+
+	regmap_read(eth->sgmiisys, SGMSYS_SGMII_MODE, &val);
+	val |= SGMII_REMOTE_FAULT_DIS;
+	regmap_write(eth->sgmiisys, SGMSYS_SGMII_MODE, val);
+
+	regmap_read(eth->sgmiisys, SGMSYS_PCS_CONTROL_1, &val);
+	val |= SGMII_AN_RESTART;
+	regmap_write(eth->sgmiisys, SGMSYS_PCS_CONTROL_1, val);
+
+	regmap_read(eth->sgmiisys, SGMSYS_QPHY_PWR_STATE_CTRL, &val);
+	val &= ~SGMII_PHYA_PWD;
+	regmap_write(eth->sgmiisys, SGMSYS_QPHY_PWR_STATE_CTRL, val);
+
+	/* Determine MUX for which GMAC uses the SGMII interface */
+	if (MTK_HAS_CAPS(eth->soc->caps, MTK_DUAL_GMAC_SHARED_SGMII)) {
+		regmap_read(eth->ethsys, ETHSYS_SYSCFG0, &val);
+		val &= ~SYSCFG0_SGMII_MASK;
+		val |= !mac_id ? SYSCFG0_SGMII_GMAC1 : SYSCFG0_SGMII_GMAC2;
+		regmap_write(eth->ethsys, ETHSYS_SYSCFG0, val);
+
+		dev_info(eth->dev, "setup shared sgmii for gmac=%d\n",
+			 mac_id);
+	}
+
+	/* Setup the GMAC1 going through SGMII path when SoC also support
+	 * ESW on GMAC1
+	 */
+	if (MTK_HAS_CAPS(eth->soc->caps, MTK_GMAC1_ESW | MTK_GMAC1_SGMII) &&
+	    !mac_id) {
+		mtk_w32(eth, 0, MTK_MAC_MISC);
+		dev_info(eth->dev, "setup gmac1 going through sgmii");
+	}
+}
 
 static void mtk_mac_config(struct net_device *ndev, unsigned int mode,
 			   const struct phylink_link_state *state)
@@ -271,15 +311,15 @@ static void mtk_mac_config(struct net_device *ndev, unsigned int mode,
 			mcr |= MAC_MCR_FORCE_DPX;
 		if (state->link)
 			mcr |= MAC_MCR_FORCE_LINK;
+		if (state->pause & MLO_PAUSE_TX)
+			mcr |= MAC_MCR_FORCE_TX_FC;
+		if (state->pause & MLO_PAUSE_RX)
+			mcr |= MAC_MCR_FORCE_RX_FC;
 	}
-	if (state->pause & MLO_PAUSE_TX)
-		mcr |= MAC_MCR_FORCE_TX_FC;
-	if (state->pause & MLO_PAUSE_RX)
-		mcr |= MAC_MCR_FORCE_RX_FC;
 
 	mtk_w32(mac->hw, mcr, MTK_MAC_MCR(mac->id));
 
-	pr_warn("mtk_mac_config: GM%d: M%d: [%i](%s) mcr:%x\n", mac->id, mode, state->interface, phy_modes(state->interface), mcr);
+	pr_warn("mtk_mac_config: GM%d: M%d: [%i](%s) mcr:%x pause=%x\n", mac->id, mode, state->interface, phy_modes(state->interface), mcr, state->pause);
 
 	return;
 }
@@ -364,24 +404,19 @@ static void mtk_validate(struct net_device *ndev, unsigned long *supported,
 		return;
 	}
 
-	/* Allow all the expected bits */
 	phylink_set(mask, Autoneg);
-	phylink_set_port_modes(mask);
 
-	/* Asymmetric pause is unsupported */
 	phylink_set(mask, Pause);
+	phylink_set(mask, Asym_Pause);
 
-	/* Half-duplex at speeds higher than 100Mbit is unsupported */
+	phylink_set(mask, MII);
+
+	phylink_set(mask, 10baseT_Half);
+	phylink_set(mask, 10baseT_Full);
+	phylink_set(mask, 100baseT_Half);
+	phylink_set(mask, 100baseT_Full);
 	phylink_set(mask, 1000baseT_Full);
-	phylink_set(mask, 1000baseX_Full);
-
-	if (!phy_interface_mode_is_8023z(state->interface)) {
-		/* 10M and 100M are only supported in non-802.3z mode */
-		phylink_set(mask, 10baseT_Half);
-		phylink_set(mask, 10baseT_Full);
-		phylink_set(mask, 100baseT_Half);
-		phylink_set(mask, 100baseT_Full);
-	}
+	phylink_set(mask, 1000baseT_Full);
 
 	bitmap_and(supported, supported, mask,
 		   __ETHTOOL_LINK_MODE_MASK_NBITS);
@@ -2328,21 +2363,6 @@ static int mtk_nway_reset(struct net_device *dev)
 		return -ENOTSUPP;
 
 	return phylink_ethtool_nway_reset(mac->phylink);
-}
-
-static u32 mtk_get_link(struct net_device *dev)
-{
-	struct mtk_mac *mac = netdev_priv(dev);
-	int err;
-
-	if (unlikely(test_bit(MTK_RESETTING, &mac->hw->state)))
-		return -EBUSY;
-
-	err = genphy_update_link(dev->phydev);
-	if (err)
-		return ethtool_op_get_link(dev);
-
-	return dev->phydev->link;
 }
 
 static void mtk_get_strings(struct net_device *dev, u32 stringset, u8 *data)
