c9e38f7067722096de7fd93326020b42297b5c4b
René van Dorst <opensource@vdorst.com>
2019-03-12 23:40:42 +0100

    dsa: mt7530: HACK: P4 connected via P5 to 2nd GMAC of soc.
    Signed-off-by: René van Dorst <opensource@vdorst.com>

--- drivers/net/dsa/mt7530.c
+++ drivers/net/dsa/mt7530.c
@@ -694,54 +694,66 @@ void mt7530_setup_port5(struct mt7530_priv *priv)
 {
 	/* Enable and setup Port 5; */
 
-	int val, hwtrap;
+	int val;
 	mutex_lock(&priv->reg_mutex);
 
-	hwtrap = mt7530_read(priv, MT7530_MHWTRAP);
+	val = mt7530_read(priv, MT7530_MHWTRAP);
 
-	hwtrap |= MHWTRAP_MANUAL | MHWTRAP_P5_MAC_SEL | MHWTRAP_P5_DIS;
+	val |= MHWTRAP_MANUAL | MHWTRAP_P5_MAC_SEL;
 
-	hwtrap &= ~MHWTRAP_P5_RGMII_MODE & ~MHWTRAP_P5_DIS & ~MHWTRAP_PHY0_SEL;
+	val &= ~MHWTRAP_P5_RGMII_MODE & ~MHWTRAP_P5_DIS & ~MHWTRAP_PHY0_SEL;
 
 	switch (priv->p5_interface) {
 	case PHY_INTERFACE_MODE_RGMII_INTERNAL_P0_AN:
-		hwtrap |= MHWTRAP_PHY0_SEL;
+		val |= MHWTRAP_PHY0_SEL;
 		/* fall-thru */
 	case PHY_INTERFACE_MODE_RGMII_INTERNAL_P4_AN:
-		hwtrap &= ~MHWTRAP_P5_MAC_SEL & ~MHWTRAP_P5_DIS; 
-		hwtrap |= MHWTRAP_P5_RGMII_MODE;
+		val &= ~MHWTRAP_P5_MAC_SEL & ~MHWTRAP_P5_DIS; 
+		val |= MHWTRAP_P5_RGMII_MODE;
 
 
 		/* P5 RGMII TX Clock Control, delay 4 */
 		mt7530_write(priv, MT7530_P5RGMIITXCR, CSR_RGMII_TXC_CFG(0x14));
 
 		/* P5 RGMII RX Clock Control: delay setting for 1000M */
-		hwtrap = CSR_RGMII_EDGE_ALIGN | CSR_RGMII_RXC_0DEG_CFG(2);
-		mt7530_write(priv, MT7530_P5RGMIIRXCR, val);
+		mt7530_write(priv, MT7530_P5RGMIIRXCR,
+			     CSR_RGMII_EDGE_ALIGN | CSR_RGMII_RXC_0DEG_CFG(2));
 
 		/* Setup the MAC by default for the cpu port */
 		mt7530_write(priv, MT7530_PMCR_P(5), 0x56300);
 		
-		pr_warn("Setup P5 HWTRAP: %x, phy-mode: PHY P%d\n", hwtrap, priv->p5_interface==PHY_INTERFACE_MODE_RGMII_INTERNAL_P0_AN ? 0 : 4);
+		pr_warn("Setup P5 HWTRAP: 0x%x, phy-mode: PHY P%d\n", val, priv->p5_interface==PHY_INTERFACE_MODE_RGMII_INTERNAL_P0_AN ? 0 : 4);
 		break;
 	case PHY_INTERFACE_MODE_RGMII:
 	case PHY_INTERFACE_MODE_RGMII_RXID:
 	case PHY_INTERFACE_MODE_RGMII_TXID:
 	case PHY_INTERFACE_MODE_RGMII_ID:
-		hwtrap |= MHWTRAP_P5_RGMII_MODE;
+		val |= MHWTRAP_P5_RGMII_MODE;
 
 		/* P5 RGMII TX Clock Control, delay 0 */
 		mt7530_write(priv, MT7530_P5RGMIITXCR, CSR_RGMII_TXC_CFG(0x14));
 
 		/* P5 RGMII RX Clock Control: delay setting for 1000M */
-		val = CSR_RGMII_EDGE_ALIGN | CSR_RGMII_RXC_0DEG_CFG(2);
-		mt7530_write(priv, MT7530_P5RGMIIRXCR, val);
+		mt7530_write(priv, MT7530_P5RGMIIRXCR,
+			     CSR_RGMII_EDGE_ALIGN | CSR_RGMII_RXC_0DEG_CFG(2));
 
-		pr_warn("Setup P5 HWTRAP: %x, phy-mode: %s\n", hwtrap, phy_modes(priv->p5_interface));
+		pr_warn("Setup P5 HWTRAP: %x, phy-mode: %s\n", val, phy_modes(priv->p5_interface));
 		break;
 	case PHY_INTERFACE_MODE_RGMII_AN:
-		mt7530_write(priv, MT7530_PMCR_P(5), 0x76300);
-		pr_warn("Setup P5 HWTRAP: %x, phy-mode: RGMII_AN EPHY\n", hwtrap);
+		val |= MHWTRAP_P5_DIS;
+		// val &= ~MHWTRAP_P5_DIS;
+		val |= MHWTRAP_P5_RGMII_MODE;
+		mt7530_write(priv, MT7530_PMCR_P(5), 0x56300); // NOT WORKING YET!
+
+		/* P5 RGMII TX Clock Control, delay 0 */
+		mt7530_write(priv, MT7530_P5RGMIITXCR, CSR_RGMII_TXC_CFG(0x14));
+
+		/* P5 RGMII RX Clock Control: delay setting for 1000M */
+		mt7530_write(priv, MT7530_P5RGMIIRXCR,
+			     CSR_RGMII_EDGE_ALIGN | CSR_RGMII_RXC_0DEG_CFG(2));
+
+		pr_warn("Setup P5 HWTRAP: %x, phy-mode: RGMII_AN EPHY\n", val);
+		break;
 	default:
 		pr_warn("%s: Unsupported p5_interface: %d\n", __func__, priv->p5_interface);
 		mutex_unlock(&priv->reg_mutex);
@@ -824,7 +836,7 @@ mt7530_port_disable(struct dsa_switch *ds, int port,
 {
 	struct mt7530_priv *priv = ds->priv;
 
-	//pr_info("mt7530_port_disable: P%d\n", port);
+	pr_info("mt7530_port_disable: P%d\n", port);
 
 	mutex_lock(&priv->reg_mutex);
 
@@ -839,10 +851,13 @@ mt7530_port_disable(struct dsa_switch *ds, int port,
 	mt7530_write(priv, MT7530_PMCR_P(port), PMCR_FORCE_LNK_DOWN);
 
 	/* Disable port 5 hardware */
+
+	/*
 	if (port == 5 && !priv->p5_interface) {
 		mt7530_rmw(priv, MT7530_MHWTRAP, 0, MHWTRAP_MANUAL | MHWTRAP_P5_DIS);
 		priv->p5_interface = PHY_INTERFACE_MODE_NA;
 	}
+	*/
 
 	mutex_unlock(&priv->reg_mutex);
 }
